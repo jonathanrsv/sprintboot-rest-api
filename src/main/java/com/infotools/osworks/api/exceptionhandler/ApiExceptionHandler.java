eddc321847879a27671a0820b2d0af91b4172301
Jonathan Rodrigues da Silva <jonathanrs@ciandt.com>
2021-01-20 09:48:56 -0300

    first commit

---
+++ src/main/java/com/infotools/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.infotools.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo("Problema com campo invalido");
+		problema.setDataHora(LocalDateTime.now());
+
+		
+		return super.handleExceptionInternal(ex, problema, headers, status, request);
+		
+	}
+	
+}
